37b8893c772414648788d273e5a08b59fa06737e
Kamal Pandey <kamalpandey0088@gmail.com>
2026-07-05 02:37:14 +0530

    fix: remove unused useGSAP import to resolve typescript compilation error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useGSAP } from '@gsap/react'
 import './index.css'
 import App from './App'
 
